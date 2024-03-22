d42858738adf7b2ce297fb816c6651fbe640c673
NidhalNaffati <nidhalnaffati@gmail.com>
2024-03-22 16:48:11 +0100

    feat: Implement Prettier formatting

--- src/route/AppRoutes.tsx
+++ src/route/AppRoutes.tsx
@@ -1,12 +1,13 @@
 import {Route, Routes} from "react-router-dom";
 import Settings from "../pages/Settings.tsx";
 import Transcription from "../pages/Transcription.tsx";
+import Home from "../pages/Home.tsx";
 
 function App() {
 	return (
 		<>
 			<Routes>
-				<Route path="/" Component={Transcription} />
+				<Route path="/" Component={Home} />
 				<Route path="/Settings" Component={Settings} />
 				<Route path="/Transcription" Component={Transcription} />
 			</Routes>
